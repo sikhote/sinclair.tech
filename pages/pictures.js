71c566b68b42802ec815ba3b184094801ac64f1b
David Sinclair <david@sinclair.tech>
2021-07-28 17:02:23 -0700

    pics

--- pages/pictures.js
+++ pages/pictures.js
@@ -1,14 +1,14 @@
 import Page from 'components/pages/Pictures';
-import path from 'path';
-import { readdirSync } from 'fs';
+// import path from 'path';
+// import { readdirSync } from 'fs';
 
-export const getServerSideProps = () => {
-  const pictures = readdirSync(
-    path.join(__dirname, '../../../public/assets/img/pictures/'),
-  )
-    .reverse()
-    .filter((picture) => picture !== '.DS_Store');
-  return { props: { pictures } };
-};
+// export const getServerSideProps = () => {
+//   const pictures = readdirSync(
+//     path.join(__dirname, '../../../public/assets/img/pictures/'),
+//   )
+//     .filter((picture) => picture !== '.DS_Store')
+//     .reverse();
+//   return { props: { pictures } };
+// };
 
 export default Page;

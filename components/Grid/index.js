2751ca6a8e18565a2814b68feb9976b58e90fcc5
David Sinclair <dsincla@rei.com>
2024-02-17 16:00:36 -0800

    pretty

--- components/Grid/index.js
+++ components/Grid/index.js
@@ -4,9 +4,7 @@ export default function Grid({ items, ParentTag = 'ul', ChildTag = 'li' }) {
   return (
     <ParentTag className={styles.grid}>
       {items.map(({ item, key }) => (
-        <ChildTag key={key}>
-          {item}
-        </ChildTag>
+        <ChildTag key={key}>{item}</ChildTag>
       ))}
     </ParentTag>
   );
